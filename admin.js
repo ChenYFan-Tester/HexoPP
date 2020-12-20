c6a3cd5d54a7ac9b1c9b799945e66f6b9983f95b
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-12-20 11:23:52 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,55 @@
-const loadMarkdown = (url) => {
+
+	function base64Encode(input){
+                var rv;
+                rv = encodeURIComponent(input);
+                rv = unescape(rv);
+                rv = window.btoa(rv);
+                return rv;
+        }
+	function ajaxObject() {
+    var xmlHttp;
+    try {
+        // Firefox, Opera 8.0+, Safari
+        xmlHttp = new XMLHttpRequest();
+        } 
+    catch (e) {
+        // Internet Explorer
+        try {
+                xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+            try {
+                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                alert("您的浏览器不支持AJAX！");
+                return false;
+            }
+        }
+    }
+    return xmlHttp;
+}
+ 
+// ajax post请求：
+function ajaxPost ( url , data , fnSucceed , fnFail , fnLoading ) {
+    var ajax = ajaxObject();
+    ajax.open( "post" , url , true );
+    ajax.setRequestHeader( "Content-Type" , "application/x-www-form-urlencoded" );
+    ajax.onreadystatechange = function () {
+        if( ajax.readyState == 4 ) {
+            if( ajax.status == 200 ) {
+                fnSucceed( ajax.responseText );
+            }
+            else {
+                fnFail( "HTTP请求错误！错误码："+ajax.status );
+            }
+        }
+        else {
+            fnLoading();
+        }
+    }
+    ajax.send( data );
+ 
+}
+	const loadMarkdown = (url) => {
                 mdeditor.value = '加载中。。。';
                 fetch(url, { method: 'GET' }).then((resp) => {
                     return Promise.all([resp.ok, resp.status, resp.text(), resp.headers]);
@@ -34,5 +85,21 @@ const loadMarkdown = (url) => {
 
 	function getdoc(){
 	url="/hpp/admin/api/getdoc/"+choo.value
-	  loadMarkdown(url);
+	loadMarkdown(url);
+	};
+	function hpp_uploadmarkdown(){
+	var ajax = ajaxObject();
+    ajax.open( "post" , '/hpp/admin/api/adddoc/'+choo.value , true );
+    ajax.setRequestHeader( "Content-Type" , "application/x-www-form-urlencoded" );
+    ajax.onreadystatechange = function () {
+        if( ajax.readyState == 4 ) {
+            if( ajax.status == 200 ) {
+                alert('200')
+            }
+            else {
+                alert('ERROR');
+            }
+        }
+    }
+    ajax.send(base64Encode(mdeditor.value));
 	}
