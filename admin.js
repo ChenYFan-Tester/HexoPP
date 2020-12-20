b412757460cf206beb63b10ea5d222fc1a67fa55
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-12-20 11:06:50 +0800

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,39 @@
+
+	var ctJson = "/hpp/admin/api/getlist"
+        $.getJSON(ctJson, function (data) {
+            $.each(data, function (index, value) {
+                $("#choo").append(`
+				  <option>${value.name}<\/option>
+                `);
+            });
+        });
+	$(function(){
+		$("#mdeditor").markdown({language:'zh'})
+	});
+
+	function getdoc(){
+	url="/hpp/admin/api/getdoc/"+choo.value
+	  const loadMarkdown = (url) => {
+                mdeditor.value = '加载中。。。';
+                fetch(url, { method: 'GET' }).then((resp) => {
+                    return Promise.all([resp.ok, resp.status, resp.text(), resp.headers]);
+                }).then(([ok, status, data, headers]) => {
+                    if (ok) {
+                        return {
+                            ok,
+                            status,
+                            data,
+                            headers
+                        }
+                    } else {
+                        throw new Error(JSON.stringify(json.error));
+                    }
+                }).then((resp) => {
+                    mdeditor.value = resp.data;
+                }).catch((error) => {
+                    mdeditor.value = '';
+                });
+            
+        };
+loadMarkdown(url);
+	}
